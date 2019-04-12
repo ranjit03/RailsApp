7ff021fbfb6b97091423b68c817d76e073cadbf1
ranjit <ranjitendurthi03@gmail.om>
2019-04-12 13:43:39 +1200

    categories controller added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
  
   root to: 'home#index'
   resources:posts
+  resources:categories 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
