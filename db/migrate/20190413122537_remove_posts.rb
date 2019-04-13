8c27a26698e6aed430f4974b62b0d3c24038d663
ranjit <ranjitendurthi03@gmail.om>
2019-04-14 02:13:06 +1200

    fixed migration files

--- db/migrate/20190413122537_remove_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemovePosts < ActiveRecord::Migration[5.2]
-  def change
-    drop_table(:posts)
-  end
-end
--- db/migrate/20190413123129_delete_tests_table.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTestsTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table(:posts)
-  end
-end
