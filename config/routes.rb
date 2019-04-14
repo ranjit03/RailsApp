926dfe6a523c9a28f58f22cd36895901b03de8eb
ranjit <ranjitendurthi03@gmail.om>
2019-04-14 19:21:50 +1200

    all routes for pages views fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,17 @@ Rails.application.routes.draw do
   get 'pages/blog'
   get 'pages/about'
   get 'pages/contact'
+  get 'home/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'home#index'
   resources:posts
   resources:categories 
+
+  get '/index', :to => 'home#index'
+  get '/about', :to => 'pages#about'
+  get '/blog', :to => 'pages#blog'
+  get '/contact', :to => 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
